389410dbc8805c87cf31a5a09b24da7a5e99cabb
Hau5ratz <16449790+Hau5ratz@users.noreply.github.com>
2018-06-21 16:42:12 -0400

    Create Download.py

---
+++ Download.py
@@ -0,0 +1,9 @@
+import requests
+import shutil
+from bs4 import beautifulsoup
+
+class download(bots.Meta):
+    '''
+    This class guesses files to download from given HTML
+    '''
+    def __init__(self,):
